ff19ab39a94b977922f28f63ca1e582b2abe2cdd
Roman <semkotove@yandex.ru>
2023-05-19 12:05:45 +0300

    Tasks 05 && 07 added

---
+++ Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if(number > 0)
+{
+    for(int i = -number; i<=number; i++)
+        Console.Write(i+" ");
+}
+else Console.Write("Number isn't natural");
---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.Write(number % 10);
+}
+else Console.Write("Wrong number");
