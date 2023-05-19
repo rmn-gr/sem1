27d0053b33b9663af73af228e6ee500cb0312af3
Roman <semkotove@yandex.ru>
2023-05-19 11:46:17 +0300

    Tasks 01 && 003 added

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Square of {number} - " + Math.Pow(number, 2));
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.Write("Enter number1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number2 == number1 / number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
