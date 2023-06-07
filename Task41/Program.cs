f781174fa73e42d684a9ea2203a9991bda6cbf0c
Roman <semkotove@yandex.ru>
2023-06-07 17:03:00 +0300

    Tasks 41, 43 have been added

---
+++ Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine();
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+
+
+
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+
+int[] CreateUserArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i+1} element: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+    }
+    return arr;
+}
+
+Console.Write("Enter size of array: ");
+int sizeOfArray = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateUserArray(5);
+PrintArray(array);
+Console.Write(" -> " + CountPositiveNumbers(array));
---
+++ Task43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double[] EnterCoefficients(string msg)
+{
+    double[] line = new double[2];
+    Console.Write($"Enter k for {msg}: ");
+    line[0] = Convert.ToDouble(Console.ReadLine());
+    Console.Write($"Enter b for {msg}: ");
+    line[1] = Convert.ToDouble(Console.ReadLine());
+    return line;
+}
+
+double[] GetCrossing(double[] ln1, double[] ln2)
+{
+    double[] crossing = new double[2];
+    crossing[0] = (ln1[1] - ln2[1]) / (ln2[0] - ln1[0]);
+    crossing[1] = (crossing[0] * ln1[0]) + ln1[1];
+    return crossing;
+}
+
+
+double[] line1 = EnterCoefficients("Line 1");
+double[] line2 = EnterCoefficients("Line 2");
+
+double[] crossingPoint = GetCrossing(line1, line2);
+
+Console.WriteLine($"({crossingPoint[0]}, {crossingPoint[1]})");
